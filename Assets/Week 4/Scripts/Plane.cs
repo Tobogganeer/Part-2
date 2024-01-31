5ba70c7f810f45fab8b717036e7b8e7983ac92b5
Tobogganeer <evandaveikis@gmail.com>
2024-01-31 13:25:45 -0500

    Task 1b: updating of LineRenderer points

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class Plane : MonoBehaviour
 {
     public List<Vector2> points;
@@ -9,24 +10,35 @@ public class Plane : MonoBehaviour
 
     Vector3 lastPosition;
     Camera cam;
+    LineRenderer lineRenderer;
 
     private void Start()
     {
         cam = Camera.main;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 0;
     }
 
     private void OnMouseDown()
     {
         points.Clear();
-        lastPosition = transform.position;
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        lastPosition = mousePos;
+
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, lastPosition);
     }
 
     private void OnMouseDrag()
     {
         Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
         if ((mousePos - lastPosition).sqrMagnitude > pointThreshold * pointThreshold)
         {
             points.Add(mousePos);
+            lineRenderer.SetPosition(lineRenderer.positionCount++, mousePos);
             lastPosition = mousePos;
         }
     }
