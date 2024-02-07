86d5df7e4c2b726c699b17e5872e8a096a5095c2
Evan Daveikis <evandaveikis@gmail.com>
2024-02-07 14:53:11 -0500

    UIBar implementation

--- Assets/Week 5/Scripts/UIBar.cs
+++ Assets/Week 5/Scripts/UIBar.cs
@@ -6,16 +6,32 @@ using UnityEngine.UI;
 public class UIBar : MonoBehaviour
 {
     public Image fill;
+    public float fillMoveSpeed = 10f;
 
-    // Start is called before the first frame update
-    void Start()
+    float fillTarget = 1f;
+    float fillAmount;
+
+    private void Start()
     {
-        
+        fillAmount = fillTarget;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Make the bar jerk towards desired fill amount
+        fillAmount = Mathf.Lerp(fillAmount, fillTarget, fillMoveSpeed * Time.deltaTime);
+        fill.fillAmount = fillAmount;
+    }
+
+    public void SetFill(float amount01, bool noLerp = false)
+    {
+        fillTarget = Mathf.Clamp(amount01, 0f, 1f);
+        if (noLerp)
+            fillAmount = fillTarget;
+    }
+
+    public void SetFill(float amount, float min, float max, bool noLerp = false)
+    {
+        SetFill(Mathf.InverseLerp(min, max, amount), noLerp);
     }
 }
