e8b86098e3a164b5f0d7ea0feb1ac5af78f3ba16
Evan Daveikis <evandaveikis@gmail.com>
2024-02-14 14:59:42 -0500

    Task 4 complete: UI menu with resolution changing buttons

--- Assets/Week 6/Scripts/ResolutionScaler.cs
+++ Assets/Week 6/Scripts/ResolutionScaler.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ResolutionScaler : MonoBehaviour
 {
+    int width, height;
+
     public void SetTo16x9()
     {
-        //SetResolution();
+        // Actually set it to 16x10
+        SetResolution(1920, 1200);
     }
 
     public void SetToFullHD()
@@ -14,7 +17,17 @@ public class ResolutionScaler : MonoBehaviour
         SetResolution(1920, 1080);
     }
 
-    void SetResolution(int width, int height)
+    public void SetWidth(int width) => this.width = width;
+    public void SetHeight(int height) => this.height = height;
+
+    public void SetResolution(int width, int height)
+    {
+        SetWidth(width);
+        SetHeight(height);
+        Apply();
+    }
+
+    public void Apply()
     {
         Screen.SetResolution(width, height, false);
     }
