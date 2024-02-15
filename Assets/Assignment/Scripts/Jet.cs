b1efbf508e0b26859a08806c7a558c2d4cacd48f
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-15 12:57:13 -0500

    Jet variables

--- Assets/Assignment/Scripts/Jet.cs
+++ Assets/Assignment/Scripts/Jet.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Jet : MonoBehaviour
 {
+    public float speed = 3f;
+    public float angularSpeed = 180f;
+
+    [Space]
+    public float boostTime = 2f;
+    public AnimationCurve boostCurve;
+    public float boostCooldown = 6.5f;
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    float boostTimer;
+    float boostCooldownTimer;
+    Quaternion targetRotation;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,3 +33,25 @@ public class Jet : MonoBehaviour
         
     }
 }
+
+/*
+
+Jet.cs (player)
+- Goes towards cursor location
+- Has a temporary speed boost on click
+- Turns to face the cursor and flies forward
+- Has limited number of lives
+- By nature of following mouse will never fly too far out of bounds
+- Rigidbody2D (kinematic) for movement and collisions
+- Some kind of 2D collider (maybe capsule)
+- Animator for plane animations (maybe wiggles wings when turning)
+- Pseudocode:
+  - Variables for speed, angularSpeed, boostTime, boostCurve, boostCooldown, boostTimer,
+  - (cont'd) rigidbody, targetRotation, animator
+  - Gather references to components in Start
+  - Update target rotation and animator in Update
+  - (cont'd) Check for boost in Update and update HUD
+  - Rotate and move in FixedUpdate
+  - Let Manager know when this is destroyed
+
+*/
