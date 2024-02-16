2f585346c3a0a2e06ab489439274563ae3a2afe5
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-16 16:38:42 -0500

    MissileSpawnArea variables

--- Assets/Assignment/Scripts/MissileSpawnArea.cs
+++ Assets/Assignment/Scripts/MissileSpawnArea.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class MissileSpawnArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Vector2 spawnArea;
+    public float actualSpawnPushback = 5f;
 }
+
+/*
+
+MissileSpawnArea.cs
+- Stores where missile warnings and missiles can be spawned
+- Just a vector2 for the area and a float for how far back missiles should be spawned
+- Pseudocode
+  - Variables for areaSize, missilePushback
+  - fn GetRandomPosition() -> returns random point in area (will be onscreen)
+  - fn GetMissilePosition(spawnPos) -> pushes spawnPos back by missilePushback (will be offscreen)
+  - Draw helpful visuals in OnDrawGizmos
+
+*/
--- Assets/Assignment/Scripts/MissileSpawner.cs
+++ Assets/Assignment/Scripts/MissileSpawner.cs
@@ -16,3 +16,21 @@ public class MissileSpawner : MonoBehaviour
         
     }
 }
+
+/*
+
+MissileSpawner.cs
+- Activates a missile warning and spawns a missile every few seconds (randomized)
+- Stores a list of spawn areas (boxes) and spawns the missile in a random area of a random box
+- No extra components (empty gameobject)
+- Pseudocode:
+  - Variables for missileSpawnTimeRange, spawnTimer, spawnAreas, missileWarningPrefab
+  - Update timer and spawn missile warning if necessary in Update (see below)
+  - fn SpawnMissileWarning() ->
+    - Pick random spawn area
+    - Pick random position from selected area (will be inside screen)
+    - Push position backwards to find missile spawn position
+    - Spawn missile warning at position (in screen), passing it the missile position
+    - Missile warning handles spawn of missile itself
+
+*/
