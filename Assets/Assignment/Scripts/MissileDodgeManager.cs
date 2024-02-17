30917e8767dc38e591a08c141283d5ae0025c720
Evan D (Tobo) <66337562+Tobogganeer@users.noreply.github.com>
2024-02-16 19:32:32 -0500

    Manager variables

--- Assets/Assignment/Scripts/MissileDodgeManager.cs
+++ Assets/Assignment/Scripts/MissileDodgeManager.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MissileDodgeManager : MonoBehaviour
 {
     public static MissileDodgeManager instance;
 
+    public float respawnTime = 5f;
+    [Range(1, 4)] public int startingLives = 3;
     public GameObject jetPrefab;
     Jet currentJet;
+
+    [Space]
+    public GameObject hud;
+    public GameObject gameOverScreen;
+    public TMP_Text respawnTimerText;
+    public TMP_Text scoreText;
+    public TMP_Text clockText;
+
+    float respawnTimer;
+    int lives;
 
     public static Jet CurrentJet => instance.currentJet;
     public static bool JetAlive => CurrentJet != null;
@@ -32,3 +45,16 @@ public class MissileDodgeManager : MonoBehaviour
         currentJet = Instantiate(jetPrefab, Vector3.zero, Quaternion.identity).GetComponent<Jet>();
     }
 }
+
+/*
+
+MissileDodgeManager.cs (various housekeeping)
+- Respawns player, forces game end, manages game flow
+- Pseudocode:
+  - Variables for playerPrefab, lives, respawnTime, respawnTimer, isPlaneAlive
+  - fn OnPlaneDestroyed() -> sets timer and isPlaneAlive bool, decreases lives
+  - Update respawn timer and corresponding text in Update
+  - (cont'd) respawn plane near middle of world when timer has elapsed and lives > 0
+  - Show game over text and force exit to main menu if lives <= 0
+
+*/
