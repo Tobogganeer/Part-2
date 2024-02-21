8be5181c4fb445be623c1165fa63253f2c232ff3
Evan Daveikis <evandaveikis@gmail.com>
2024-02-21 14:45:10 -0500

    Task 4b complete: Goalkeeper moves smoothly and looks towards target

--- Assets/Week 7/Scripts/GoalkeeperController.cs
+++ Assets/Week 7/Scripts/GoalkeeperController.cs
@@ -9,11 +9,13 @@ public class GoalkeeperController : MonoBehaviour
     public Rigidbody2D goalkeeperRB;
     public float goalRadius = 2.5f;
     public float moveSpeed = 1f;
+    public float rotateSpeed = 360f;
 
     private void FixedUpdate()
     {
         // Default target is the centre of the goal
         Vector2 targetPosition = transform.position;
+        Vector2 lookDir = Vector2.down;
 
         if (Controller.SelectedPlayer != null)
         {
@@ -21,6 +23,7 @@ public class GoalkeeperController : MonoBehaviour
             Vector2 playerPos = Controller.SelectedPlayer.transform.position;
             Vector2 centerPos = transform.position;
             Vector2 direction = playerPos - centerPos;
+            lookDir = direction;
             float distance = direction.magnitude;
 
             // Set the target position (according to the diagram)
@@ -28,7 +31,13 @@ public class GoalkeeperController : MonoBehaviour
         }
 
         //goalkeeperRB.position = targetPosition;
-        goalkeeperRB.position = Vector2.MoveTowards(goalkeeperRB.position, targetPosition, moveSpeed * Time.deltaTime);
+        goalkeeperRB.MovePosition(Vector2.MoveTowards(goalkeeperRB.position, targetPosition, moveSpeed * Time.deltaTime));
+
+        // Convert target direction into a quaternion
+        float targetAngle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        Quaternion targetRot = Quaternion.Euler(0, 0, targetAngle + 90f); // + 90 because our goalkeeper is upside down
+        // Yes I could rotate the sprite around again but oh well
+        goalkeeperRB.MoveRotation(Quaternion.RotateTowards(goalkeeperRB.transform.rotation, targetRot, rotateSpeed * Time.deltaTime));
     }
 
     private void OnDrawGizmos()
